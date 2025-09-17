2035855e5decadb2d16903b4b1dde3d8b16119ab
pcfrati <fratipaula@gmail.com>
2025-09-17 15:48:09 -0300

    17.09.25

---
+++ firebase/todo_list_firebase/README.md
@@ -0,0 +1,31 @@
+# Estrutura do Banco de Dados
+
+## Dentro do Firebase -> Banco de Dados Vertical
+
+DB(default)
+{
+    "usuarios":[
+        "user1":{
+            "tarefas":[
+                {
+                    "id":"asd123",
+                    "titulo":"a",
+                    "concluida":false,
+                    "dataCriacao":12345678
+                },
+                {
+                    "id":"asd123",
+                    "titulo":"a",
+                    "concluida":false,
+                    "dataCriacao":12345678
+                }
+            ]
+        "user2":{
+            "tarefas":[
+
+            ]
+        }
+            
+        }
+    ]
+}
--- firebase/todo_list_firebase/lib/views/tarefas_view.dart
+++ firebase/todo_list_firebase/lib/views/tarefas_view.dart
@@ -11,28 +11,47 @@ class TarefasView extends StatefulWidget {
 
 class _TarefasViewState extends State<TarefasView> {
   //& atributos
-  final FirebaseFirestore _db = FirebaseFirestore.instance; //* controlador do Cloud_Store
-  final User? _user = FirebaseAuth.instance.currentUser; //* pega as informações do Usuário Conectado
+  final FirebaseFirestore _db =
+      FirebaseFirestore.instance; //* controlador do Cloud_Store
+  final User? _user = FirebaseAuth
+      .instance
+      .currentUser; //* pega as informações do Usuário Conectado
   final TextEditingController _tarefaField = TextEditingController();
 
   //? adicionar tarefa
   void _addTarefa() async {
-    if(_tarefaField.text.trim().isNotEmpty && _user!=null){
-      await _db.collection('users').
-      doc(_user!.uid).
-      collection("tarefas").
-      add({
-        "titulo":_tarefaField.text.trim(),
-        "concluida":false,
-        "criadaEm":Timestamp.now();
-      });
-      _tarefaField.clear();
-    }
+    if (_tarefaField.text.trim().isEmpty)
+      return; //return null => interromper o método
+    //adicionar a tarefa no firestore (BD)
+    await _db.collection("usuarios").doc(_user!.uid).collection("tarefas").add({
+      "titulo": _tarefaField.text.trim(),
+      "concluida": false,
+      "dataCriacao": Timestamp.now(), //data e hora em miliseconds
+    });
   }
 
   //? atualizar status da tarefa
+  void _atualizarTarefa(String tarefaID, bool status) async {
+    await _db
+        .collection("usuarios")
+        .doc(_user!.uid)
+        .collection("tarefas")
+        .doc(tarefaID)
+        .update({"concluida": !status});
+  }
 
   //! deletar tarefa
+  void _deleteTarefa(String tarefaID) async {
+    await _db
+        .collection("usuarios")
+        .doc(_user!.uid)
+        .collection("tarefas")
+        .doc(tarefaID)
+        .delete();
+  }
+
+  //* método para pegar as tarefas -> build da tela
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,9 +59,78 @@ class _TarefasViewState extends State<TarefasView> {
         title: Text("Minha Tarefas"),
         actions: [
           IconButton(
-            onPressed: () async {await FirebaseAuth.instance.signOut();}, 
-            icon: Icon(Icons.logout))
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(Icons.logout),
+          ),
         ],
+      ),
+      //* construir a lisa de tarefas, mas se tiver a lista
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: _tarefaField,
+              decoration: InputDecoration(
+                labelText: "Nova Tarefa",
+                suffix: IconButton(
+                  onPressed: _addTarefa,
+                  icon: Icon(Icons.add, color: Colors.green),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            //* inserir a lista de tarefas do usuário
+            // o que será exibido depende d abusca no firestore
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _db
+                    .collection("usuarios")
+                    .doc(_user!.uid)
+                    .collection("tarefas")
+                    .orderBy("dataCriacao", descending: true)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return Center(child: Text("Sem Tarefa Por Enquanto"));
+                  }
+                  //* exibir as tarefas
+                  final tarefas = snapshot
+                      .data!
+                      .docs; // adiciona a busca ao vetor de tarefas
+                  return ListView.builder(
+                    itemCount: tarefas.length,
+                    itemBuilder: (context, index) {
+                      final tarefa = tarefas[index]; // tarefas em formato json
+                      //converter json => Map<string, dynamic>
+                      final tarefaMap = tarefa.data() as Map<String, dynamic>;
+                      bool concluida = tarefaMap["concluida"] == true ? true : false;
+                      
+                      return ListTile(
+                        title: Text(tarefaMap["titulo"]),
+                        leading: Checkbox(
+                          value: concluida, 
+                          onChanged: (value){
+                            setState(() {
+                              _atualizarTarefa(tarefa.id, concluida);
+                            });
+                          }),
+                        trailing: IconButton(
+                          onPressed: ()=> _deleteTarefa(tarefa.id), 
+                          icon: Icon(Icons.delete, color: Colors.red,)),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
